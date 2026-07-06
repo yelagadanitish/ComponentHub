68486726d9a0a7e7ab5f8444847901f9b50675b9
Nitish <yelagadanitish@gmail.com>
2026-07-06 17:08:21 +0530

    Temporary rename Sidebar

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/navbar/Navbar";
-import Sidebar from "../components/sidebar/Sidebar";
+import Sidebar from "../components/sidebar/SidebarTemp";
 
 function MainLayout({ children }) {
 
